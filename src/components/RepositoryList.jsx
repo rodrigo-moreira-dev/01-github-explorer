1c4dcf30959728808aaca1ee43ed3333c52805f6
moreiraConcepta <rmoreira@conceptainvest.com>
2022-05-04 17:11:30 -0300

    adiciona componente repository list

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,16 @@
+const repositoryName = 'unform';
+
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+            <ul>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in ReactJS</p>
+                    <a href="">Acessar repositório</a>
+                </li>
+            </ul>
+        </section>
+    );
+}

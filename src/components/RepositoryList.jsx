d0afdaabea9dc913d95c9582c3dc12eb24ce1aef
moreiraConcepta <rmoreira@conceptainvest.com>
2022-05-05 13:23:18 -0300

    adiciona use effect e itera repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,17 +1,28 @@
 import { RepositoryItem } from "./RepositoryItem";
+import { useState, useEffect } from 'react';
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://www.github.com',
-};
+import '../styles/repositories.scss';
+
+// https://api.github.com/orgs/rocketseat/repos
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data));
+    }, [])
+
+    console.log(repositories);
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                <RepositoryItem repository={repository} />
+                {repositories.map(repository => (
+                    <RepositoryItem key={repository.id} repository={repository} />
+                ))}
             </ul>
         </section>
     );
